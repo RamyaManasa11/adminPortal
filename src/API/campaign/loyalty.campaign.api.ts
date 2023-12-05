5f4aeffe2d872f322dd361c7001a89149537f04c
B Ramya Manasa <bharatkumar.nettem@hcl.com>
2023-12-05 18:13:16 +0530

    env file updated

--- src/API/campaign/loyalty.campaign.api.ts
+++ src/API/campaign/loyalty.campaign.api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const loyaltyCampaignApi = {
     getCampainList: async () => {
         try {
-            const res = await axios.get(process.env.LOYALTY_MANAGEMENT_STAGE_URL+'v1/campaign/getCampaignList', {
+            const res = await axios.get(process.env.REACT_APP_LOYALTY_API+'v1/campaign/getCampaignList', {
                 headers: {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
@@ -18,7 +18,7 @@ const loyaltyCampaignApi = {
     },
     getCampainTypes: async () => {
         try {
-            const res = await axios.get(process.env.LOYALTY_MANAGEMENT_STAGE_URL+"v1/campaign/campaignType", {
+            const res = await axios.get(process.env.REACT_APP_LOYALTY_API+'v1/campaign/campaignType', {
                 headers: {
                     'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': '*',
@@ -34,7 +34,7 @@ const loyaltyCampaignApi = {
     createCampaign: async (payload: any) => {
         console.log(payload, "ActivePayload")
         try {
-            const res = await axios.post(process.env.LOYALTY_MANAGEMENT_STAGE_URL+'v1/campaign/createCampaign',
+            const res = await axios.post(process.env.REACT_APP_LOYALTY_API+'v1/campaign/createCampaign',
                 payload,
                 {
                     headers: {
@@ -52,7 +52,7 @@ const loyaltyCampaignApi = {
     },
     getCampainDetails: async (input: string) => {
         try {
-            const res = await axios.get(process.env.LOYALTY_MANAGEMENT_STAGE_URL+'v1/campaign/campaignDetails?campaignAddress=' + input,
+            const res = await axios.get(process.env.REACT_APP_LOYALTY_API+'v1/campaign/campaignDetails?campaignAddress=' + input,
                 {
                     headers: {
                         'Content-Type': 'application/json',
